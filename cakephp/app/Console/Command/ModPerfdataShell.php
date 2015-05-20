9274125bda34d1c5af6dac0fb9d512e9a7be063f
nook24 <info@nook24.eu>
2015-05-20 22:14:25 +0200

    Perfdata processor for statusengine

--- cakephp/app/Console/Command/ModPerfdataShell.php
+++ cakephp/app/Console/Command/ModPerfdataShell.php
@@ -52,7 +52,7 @@
 
 class ModPerfdataShell extends AppShell{
 	
-	public $tasks = ['Logfile'];
+	public $tasks = ['Logfile', 'Perfdata'];
 	
 	//Some class variables
 	protected $worker = null;
@@ -60,14 +60,6 @@ class ModPerfdataShell extends AppShell{
 	private $childPids = [];
 	
 	public $Config = [];
-	
-	public $servicestate = [
-		0 => 'OK',
-		1 => 'WARNING', 
-		2 => 'CRITICAL',
-		3 => 'UNKNOWN'
-	];
-	
 	
 	/**
 	 * Gets called if a user run the shell over Console/cake mod_perfdata
@@ -95,9 +87,11 @@ class ModPerfdataShell extends AppShell{
 		}
 		
 		//Some testing perfdata
-		//debug($this->parsePerfdataString('rta=0.069000ms;100.000000;500.000000;0.000000 pl=0%;20;60;0'));die();
-		//debug($this->parsePerfdataString('active=650;jobs=650;worker=3436;queues=29'));die();
-		//debug($this->parseCheckCommand('84084403-5c21-4273-835b-d8ac770b4a9f!7.0,6.0,5.0!10.0,7.0,6.0'));die();
+		//debug($this->Perfdata->parsePerfdataString('rta=0.069000ms;100.000000;500.000000;0.000000 pl=0%;20;60;0'));die();
+		//debug($this->Perfdata->parsePerfdataString('active=650;jobs=650;worker=3436;queues=29'));die();
+		//debug($this->Perfdata->parseCheckCommand('84084403-5c21-4273-835b-d8ac770b4a9f!7.0,6.0,5.0!10.0,7.0,6.0'));die();
+		
+		$this->Perfdata->init($this->Config, $this->Logfile);
 		
 		$this->forkWorkers();
 		
@@ -276,163 +270,14 @@ class ModPerfdataShell extends AppShell{
 		
 		$parsedPerfdataString = $this->parsePerfdataFileTemplate($stringFormModGearman);
 		if(isset($parsedPerfdataString['SERVICEPERFDATA'])){
-			$parsedPerfdata = $this->parsePerfdataString($parsedPerfdataString['SERVICEPERFDATA']);
-			$rrdReturn = $this->writeToRrd($parsedPerfdataString, $parsedPerfdata);
+			$parsedPerfdata = $this->Perfdata->parsePerfdataString($parsedPerfdataString['SERVICEPERFDATA']);
+			$rrdReturn = $this->Perfdata->writeToRrd($parsedPerfdataString, $parsedPerfdata);
 			if($this->Config['XML']['write_xml_files'] === true){
-				$this->updateXml($parsedPerfdataString, $parsedPerfdata, $rrdReturn);
+				$this->Perfdata->updateXml($parsedPerfdataString, $parsedPerfdata, $rrdReturn);
 			}
 		}
 	}
 	
-	public function writeToRrd($parsedPerfdataString, $parsedPerfdata){
-		if(!is_dir($this->Config['PERFDATA']['dir'].$parsedPerfdataString['HOSTNAME'])){
-			mkdir($this->Config['PERFDATA']['dir'].$parsedPerfdataString['HOSTNAME']);
-		}
-		
-		$perfdataFile = $this->Config['PERFDATA']['dir'].$parsedPerfdataString['HOSTNAME'].'/'.$parsedPerfdataString['SERVICEDESC'].'.rrd';
-		$error = '';
-		$return = true;
-		
-		if(file_exists($perfdataFile)){
-			$options = [];
-			
-			$options[] = $parsedPerfdataString['TIMET'];
-			
-			foreach($parsedPerfdata as $ds => $data){
-				$options[] = $data['current'];
-			}
-
-			
-			if($this->Config['RRDCACHED']['use'] === true){
-				if(!rrd_update($perfdataFile, [implode(':', $options)])){
-					$this->out('Error on updating RRD');
-					$return = false;
-					$error = rrd_error();
-					$this->Logfile->stlog($error);
-					//debug($error);
-				}
-			}else{
-				if(!rrd_update($perfdataFile, [implode(':', $options), '--daemon='.$this->Config['RRDCACHED']['sock']])){
-					$this->out('Error on updating RRD');
-					$return = false;
-					$error = rrd_error();
-					$this->Logfile->stlog($error);
-					//debug($error);
-				}
-			}
-		}else{
-			//RRA:AVERAGE:0.5:1:576000 RRA:MAX:0.5:1:576000 RRA:MIN:0.5:1:576000 DS:1:GAUGE:8460:U:U --start=1431375240 --step=60
-			//RRA:AVERAGE:0.5:1:576000 RRA:MAX:0.5:1:576000 RRA:MIN:0.5:1:576000 DS:1:GAUGE:8460:U:U DS:2:GAUGE:8460:U:U --start=1431375345 --step=60
-			$options = [];
-			$options[] = 'RRA:AVERAGE:'.$this->Config['RRA']['average'];
-			$options[] = 'RRA:MAX:'.$this->Config['RRA']['max'];
-			$options[] = 'RRA:MIN:'.$this->Config['RRA']['min'];
-			
-			$dataSourceCount = 1;
-			foreach($parsedPerfdata as $ds => $data){
-				if(isset($this->Config['RRD']['DATATYPE'][$data['unit']])){
-					$options[] = 'DS:'.$dataSourceCount.':'.$this->Config['RRD']['DATATYPE'][$data['unit']].':8460:U:U';
-				}else{
-					$options[] = 'DS:'.$dataSourceCount.':'.$this->Config['RRD']['DATATYPE']['default'].':8460:U:U';
-				}
-				$dataSourceCount++;
-			}
-			
-			$options[] = '--start='.$parsedPerfdataString['TIMET'];
-			$options[] = '--step='.$this->Config['RRA']['step'];
-			
-			if(!rrd_create($perfdataFile, $options)){
-				$this->out('Error on createing RRD');
-				$return = false;
-				$error = rrd_error();
-				$this->Logfile->stlog($error);
-				//debug($error);
-			}
-		}
-		
-		return [
-			'return' => $return,
-			'error' => $error
-		];
-		
-	}
-	
-	public function updateXML($parsedPerfdataString, $parsedPerfdata, $rrdReturn){
-		$xmlFile = new File($this->Config['PERFDATA']['dir'].$parsedPerfdataString['HOSTNAME'].'/'.$parsedPerfdataString['SERVICEDESC'].'.xml');
-		if(!$xmlFile->exists()){
-			$xmlFile->create();
-		}
-		
-		if($this->Config['XML']['delay'] > 0){
-			if((time() - $xmlFile->lastChange()) < $this->Config['XML']['delay']){
-				return false;
-			}
-		}
-		
-		$xml = "";
-
-$xml .= "<?xml version=\"1.0\" encoding=\"UTF-8\" standalone=\"yes\"?>
-<NAGIOS>";
-$dataSourceCounter = 1;
-$template = $this->parseCheckCommand($parsedPerfdataString['SERVICECHECKCOMMAND'])[0];
-foreach($parsedPerfdata as $ds => $data){
-$xml.="  <DATASOURCE>
-    <TEMPLATE>".$template."</TEMPLATE>
-    <RRDFILE>".$this->Config['PERFDATA']['dir'].$parsedPerfdataString['HOSTNAME'].'/'.$parsedPerfdataString['SERVICEDESC'].".rrd</RRDFILE>
-    <RRD_STORAGE_TYPE>SINGLE</RRD_STORAGE_TYPE>
-    <RRD_HEARTBEAT>".$this->Config['RRD']['heartbeat']."</RRD_HEARTBEAT>
-    <IS_MULTI>0</IS_MULTI>
-    <DS>".$dataSourceCounter."</DS>
-    <NAME>".$ds."</NAME>
-    <LABEL>".$ds."</LABEL>
-    <UNIT>".$data['unit']."</UNIT>
-    <ACT>".$data['current']."</ACT>
-    <WARN>".$data['warning']."</WARN>
-    <WARN_MIN></WARN_MIN>
-    <WARN_MAX></WARN_MAX>
-    <WARN_RANGE_TYPE></WARN_RANGE_TYPE>
-    <CRIT>".$data['critical']."</CRIT>
-    <CRIT_MIN></CRIT_MIN>
-    <CRIT_MAX></CRIT_MAX>
-    <CRIT_RANGE_TYPE></CRIT_RANGE_TYPE>
-    <MIN>".$data['min']."</MIN>
-    <MAX>".$data['max']."</MAX>
-  </DATASOURCE>";
-  $dataSourceCounter++;
-}
-
-$xml.="  <RRD>
-    <RC>".(int)$rrdReturn['return']."</RC>
-    <TXT>".($rrdReturn['return']?'successful updated':$rrdReturn['error'])."</TXT>
-  </RRD>
-  <NAGIOS_AUTH_HOSTNAME></NAGIOS_AUTH_HOSTNAME>
-  <NAGIOS_AUTH_SERVICEDESC></NAGIOS_AUTH_SERVICEDESC>
-  <NAGIOS_CHECK_COMMAND>".$parsedPerfdataString['SERVICECHECKCOMMAND']."</NAGIOS_CHECK_COMMAND>
-  <NAGIOS_DATATYPE>".$parsedPerfdataString['DATATYPE']."</NAGIOS_DATATYPE>
-  <NAGIOS_DISP_HOSTNAME>".$parsedPerfdataString['HOSTNAME']."</NAGIOS_DISP_HOSTNAME>
-  <NAGIOS_DISP_SERVICEDESC>".$parsedPerfdataString['SERVICEDESC']."</NAGIOS_DISP_SERVICEDESC>
-  <NAGIOS_HOSTNAME>".$parsedPerfdataString['HOSTNAME']."</NAGIOS_HOSTNAME>
-  <NAGIOS_HOSTSTATE></NAGIOS_HOSTSTATE>
-  <NAGIOS_HOSTSTATETYPE></NAGIOS_HOSTSTATETYPE>
-  <NAGIOS_MULTI_PARENT></NAGIOS_MULTI_PARENT>
-  <NAGIOS_PERFDATA>".$parsedPerfdataString['SERVICEPERFDATA']."</NAGIOS_PERFDATA>
-  <NAGIOS_RRDFILE>".$this->Config['PERFDATA']['dir'].$parsedPerfdataString['HOSTNAME'].'/'.$parsedPerfdataString['SERVICEDESC'].".rrd</NAGIOS_RRDFILE>
-  <NAGIOS_SERVICECHECKCOMMAND>".$parsedPerfdataString['SERVICECHECKCOMMAND']."</NAGIOS_SERVICECHECKCOMMAND>
-  <NAGIOS_SERVICEDESC>".$parsedPerfdataString['SERVICEDESC']."</NAGIOS_SERVICEDESC>
-  <NAGIOS_SERVICEPERFDATA>".$parsedPerfdataString['SERVICEPERFDATA']."</NAGIOS_SERVICEPERFDATA>
-  <NAGIOS_SERVICESTATE>OK</NAGIOS_SERVICESTATE>
-  <NAGIOS_SERVICESTATETYPE>".($parsedPerfdataString['SERVICESTATETYPE'] == 1 ? 'HARD' : 'SOFT')."</NAGIOS_SERVICESTATETYPE>
-  <NAGIOS_TIMET>".$parsedPerfdataString['TIMET']."</NAGIOS_TIMET>
-  <NAGIOS_XMLFILE>".$this->Config['PERFDATA']['dir'].$parsedPerfdataString['HOSTNAME'].'/'.$parsedPerfdataString['SERVICEDESC'].".xml</NAGIOS_XMLFILE>
-  <XML>
-   <VERSION>4</VERSION>
-  </XML>
-</NAGIOS>";
-
-	$xmlFile->write($xml);
-	$xmlFile->close();
-
-	}
 	
 	/**
 	 * Parse perfdata out of service_perfdata_file_template defined in naemon.cfg to an array
@@ -456,93 +301,4 @@ $xml.="  <RRD>
 		return $parsedData;
 	}
 	
-	/**
-	 * Parse perfdata of the naemon plugin output to an array
-	 *
-	 * @since 1.0.0
-	 * @author Daniel Ziegler <daniel@statusengine.org>
-	 *
-	 * @return void
-	 */
-	function parsePerfdataString($perfdataString){
-		/* $perfdataString should looks like this
-		rta=0.069000ms;100.000000;500.000000;0.000000 pl=0%;20;60;0
-		 */
-		$perfdata = [];
-		$arrayKeys = [
-			'current', 'unit', 'warning', 'critical', 'min', 'max'
-		];
-		
-		$defaultValues = [
-			'current' => null,
-			'unit' => null,
-			'warning' => null,
-			'critical' => null,
-			'min' => null,
-			'max' => null
-		];
-		
-		foreach(explode(" ", $perfdataString) as $dataSource){
-			$i = 2;
-			foreach(explode(';', $dataSource) as $value){
-				if(preg_match('/=/', $value)){
-					$s = preg_split('/=/', $value);
-					//Fetch unit
-					$current = '';
-					$unit = '';
-					foreach(str_split($s[1]) as $char ){
-						if( $char == '.' || $char == ',' || ($char >= '0' && $char <= '9') ){
-							$current .= $char;
-						}else{
-							$unit .= $char;
-						}
-					}
-					
-					if($unit == '%'){
-						$unit = '%%';
-					}
-					
-					$perfdata[$s[0]][$arrayKeys[0]] = str_replace(',', '.', $current);
-					$perfdata[$s[0]][$arrayKeys[1]] = $unit;
-					continue;
-				}
-				
-				$perfdata[$s[0]][$arrayKeys[$i]] = $value;
-				$i++;
-			}
-			unset($s);
-		}
-		
-		//Fil up missing fields in array
-		foreach($perfdata as $dataSource => $values){
-			$perfdata[$dataSource] = array_merge($defaultValues, $perfdata[$dataSource]);
-		}
-
-		return $perfdata;
-	}
-	
-	/**
-	 * Parse the check_command string into command_name and command_arg
-	 *
-	 * @since 1.0.0
-	 * @author Daniel Ziegler <daniel@statusengine.org>
-	 *
-	 * @param  string checkCommand from $payload
-	 * @return array  [0] => 'lan_ping', [1] => '!80!80'
-	 */
-	public function parseCheckCommand($checkCommand){
-		$cc = explode('!', $checkCommand, 2);
-		$return = [];
-		if(isset($cc[0])){
-			$return[0] = $cc[0];
-		}else{
-			$return[0] = '';
-		}
-		if(isset($cc[1])){
-			$return[1] = $cc[1];
-		}else{
-			$return[1] = '';
-		}
-		return $return;
-	}
 }
