45ba3b9979fc53594bb500f7240be39ca0e1683a
nook24 <info@nook24.eu>
2016-07-19 19:55:10 +0200

    Add header files for naemon 1.0.5

---
+++ statusengine/src/naemon-1-0-5/buildopts.h
@@ -0,0 +1,9 @@
+#ifndef NAEMON_BUILDOPTS_H__
+#define NAEMON_BUILDOPTS_H__
+#define NAEMON_SYSCONFDIR "/opt/naemon-1-0-5/etc/naemon"
+#define NAEMON_LOCALSTATEDIR "/opt/naemon-1-0-5/var"
+#define NAEMON_LOGDIR "/opt/naemon-1-0-5/var/log/naemon"
+#define NAEMON_LOCKFILE "/opt/naemon-1-0-5/var/cache/naemon/naemon.pid"
+#define NAEMON_GROUP "naemon"
+#define NAEMON_USER "naemon"
+#endif
