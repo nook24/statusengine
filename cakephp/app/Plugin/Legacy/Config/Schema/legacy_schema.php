ad5879aa2b4eaf98236f17730374928b11c70cb6
Daniel Ziegler <info@nook24.eu>
2016-07-21 10:35:33 +0200

    update database schema

--- cakephp/app/Plugin/Legacy/Config/Schema/legacy_schema.php
+++ cakephp/app/Plugin/Legacy/Config/Schema/legacy_schema.php
@@ -103,8 +103,8 @@ class LegacySchema extends CakeSchema {
 		'configfilevariable_id' => array('type' => 'integer', 'null' => false, 'default' => null, 'unsigned' => false, 'key' => 'primary'),
 		'instance_id' => array('type' => 'integer', 'null' => false, 'default' => '0', 'length' => 6, 'unsigned' => false, 'key' => 'index'),
 		'configfile_id' => array('type' => 'integer', 'null' => false, 'default' => '0', 'unsigned' => false),
-		'varname' => array('type' => 'string', 'null' => false, 'length' => 64, 'collate' => 'utf8_swedish_ci', 'charset' => 'utf8'),
-		'varvalue' => array('type' => 'string', 'null' => false, 'collate' => 'utf8_swedish_ci', 'charset' => 'utf8'),
+		'varname' => array('type' => 'string', 'null' => false, 'length' => 255, 'collate' => 'utf8_swedish_ci', 'charset' => 'utf8'),
+		'varvalue' => array('type' => 'string', 'null' => false, 'collate' => 'utf8_swedish_ci', 'charset' => 'utf8', 'length' => 1024),
 		'indexes' => array(
 			'PRIMARY' => array('column' => 'configfilevariable_id', 'unique' => 1),
 			'instance_id' => array('column' => array('instance_id', 'configfile_id'), 'unique' => 0)
